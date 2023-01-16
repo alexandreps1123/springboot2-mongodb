7373cb13f237adb6d3db4c8faf947a4ab67477ad
alexandreps1123 <alexandre.ps1123@gmail.com>
2023-01-16 10:51:25 -0300

    Obtendo um usuario por id

--- src/main/java/com/parkus/sbmongo/services/UserService.java
+++ src/main/java/com/parkus/sbmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.parkus.sbmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.parkus.sbmongo.domain.User;
 import com.parkus.sbmongo.repository.UserRepository;
+import com.parkus.sbmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,11 @@ public class UserService {
 
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> user = repository.findById(id);
+
+        return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
